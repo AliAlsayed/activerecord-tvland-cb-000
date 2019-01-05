f45fc0c438598fc6af408ef6b7a44f359d45a7e8
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-05 08:48:27 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_characters_to_actor.rb
+++ db/migrate/006_add_characters_to_actor.rb
@@ -1,4 +1,4 @@
-class AddActoridToCharater < ActiveRecord::Migration[4.2]
+class AddCharactersToActor < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :actor_id, :integer
   end
